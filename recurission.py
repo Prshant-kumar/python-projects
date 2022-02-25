e1c89cab1a1b478d451cf1635e26d6e443659c47
Prashant Kumar <prashantpk7055@gmali.com>
2022-02-25 23:33:13 +0530

    wipro Q2 added

--- recurission.py
+++ recurission.py
@@ -1,1 +1,26 @@
-print("added content")
+print("wipro coding question")
+"""
+print the total number of positive numbers in a space sepreted list
+
+Include 0 too
+
+
+"""
+from builtins import len
+
+
+n = int(input())
+arr = [int(x) for x in input().split()]
+def right(arr):
+    ans = 0
+    if len(arr) == 0:
+        return 0
+    else:
+        for x in arr:
+            if x >= 0:
+                ans = ans + 1
+    return ans
+
+
+result = right(arr)
+print(result)
